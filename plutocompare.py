e6b8713bd62d0394c5c38a0251e6ad9168d28b47
Lucian Reynolds <lucianreynolds@gmail.com>
2017-04-30 21:42:11 +0000

    added program index

---
+++ plutocompare.py
@@ -0,0 +1,29 @@
+#python3
+#this program compares the current
+#Pluto CSV with the previous to
+#discover changes, which are then
+#documented in a txt file.
+
+import csv
+
+#TODO: Check DCP for an updated Pluto data package.
+    #e.g. last download was 16v2 and now 16v3 exists.
+
+#TODO: Download Pluto zip archive.
+
+#TODO: Extract MN.csv from archive and save to a
+    #subfolder in /Pluto that is named after the
+    #new version (e.g. 16v3)
+
+#TODO: Rename the new MN.csv to the version (e.g. 16v3.csv)
+
+#TODO: Make a note in the logfile:
+    #D/L date, version, size
+
+#TODO: Load ultimate and penultimate CSV files into lists
+
+#TODO: Compare each lot and write changes to a new list.
+
+#TODO: Write changes to a new file.
+
+#TODO: Update the logfile with number of changes per CD
